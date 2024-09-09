c63167d0dcfddeaabff17d0b53b8f49cb0a47aad
MajemiteJames <majemiteokoro@gmail.com>
2024-09-09 17:49:36 +0100

    made con adjustment

--- ussd/src/ussd/ussd.service.ts
+++ ussd/src/ussd/ussd.service.ts
@@ -26,15 +26,12 @@ export class UssdService {
     switch (text) {
       case '':
         // First request. We start the response with CON
-        response = `CON What would you like to check
-        1. My account
-        2. My phone number`;
+        response = `CON What would you like to check?\n1. My account\n2. My phone number`;
         break;
 
       case '1':
         // Business logic for first-level response
-        response = `CON Choose account information you want to view
-        1. Account number`;
+        response = `CON Choose account information you want to view:\n1. Account number`;
         break;
 
       case '2':
@@ -55,6 +52,6 @@ export class UssdService {
         response = `END Invalid selection`;
     }
 
-    return response;
+    return response.trim(); // Ensure no extra white spaces
   }
 }
