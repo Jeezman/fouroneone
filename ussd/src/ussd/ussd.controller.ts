5c9f591ed35056f5cab97f71ce93084367543eff
MajemiteJames <majemiteokoro@gmail.com>
2024-09-09 14:24:37 +0100

    sample ussd

--- ussd/src/ussd/ussd.controller.ts
+++ ussd/src/ussd/ussd.controller.ts
@@ -1,34 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { UssdService } from './ussd.service';
-import { CreateUssdDto } from './dto/create-ussd.dto';
-import { UpdateUssdDto } from './dto/update-ussd.dto';
 
 @Controller('ussd')
 export class UssdController {
   constructor(private readonly ussdService: UssdService) {}
 
   @Post()
-  create(@Body() createUssdDto: CreateUssdDto) {
-    return this.ussdService.create(createUssdDto);
-  }
+  handleUssd(@Body() ussdBody: any, @Res() res) {
+    const { text, sessionId, phoneNumber } = ussdBody;
 
-  @Get()
-  findAll() {
-    return this.ussdService.findAll();
-  }
+    const response = this.ussdService.processUssd(text, sessionId, phoneNumber);
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ussdService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUssdDto: UpdateUssdDto) {
-    return this.ussdService.update(+id, updateUssdDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ussdService.remove(+id);
+    res.header('Content-Type', 'text/plain');
+    return res.send(response);
   }
 }
--- ussd/src/ussd/ussd.service.ts
+++ ussd/src/ussd/ussd.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUssdDto } from './dto/create-ussd.dto';
-import { UpdateUssdDto } from './dto/update-ussd.dto';
+import africastalking from 'africastalking';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class UssdService {
-  create(createUssdDto: CreateUssdDto) {
-    return 'This action adds a new ussd';
+  private africasTalking: any;
+
+  constructor(
+    private configService: ConfigService,
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {
+    this.africasTalking = africastalking({
+      apiKey: this.configService.get<string>('AFRICASTALKING_API_KEY'), // Access the API key
+      username: this.configService.get<string>('AFRICASTALKING_USERNAME'), // Access the username
+    });
   }
 
-  findAll() {
-    return `This action returns all ussd`;
-  }
+  // Process USSD requests
+  async processUssd(text: string, sessionId: string, phoneNumber: string) {
+    let response = '';
 
-  findOne(id: number) {
-    return `This action returns a #${id} ussd`;
-  }
+    switch (text) {
+      case '':
+        // First request. We start the response with CON
+        response = `CON What would you like to check
+        1. My account
+        2. My phone number`;
+        break;
 
-  update(id: number, updateUssdDto: UpdateUssdDto) {
-    return `This action updates a #${id} ussd`;
-  }
+      case '1':
+        // Business logic for first-level response
+        response = `CON Choose account information you want to view
+        1. Account number`;
+        break;
 
-  remove(id: number) {
-    return `This action removes a #${id} ussd`;
+      case '2':
+        // Business logic for first-level response
+        // This is a terminal request. We start the response with END
+        response = `END Your phone number is ${phoneNumber}`;
+        break;
+
+      case '1*1':
+        // Second-level response where the user selected 1 in the first instance
+        const accountNumber = 'ACC100101';
+        // This is a terminal request. We start the response with END
+        response = `END Your account number is ${accountNumber}`;
+        break;
+
+      default:
+        // Handle unexpected input
+        response = `END Invalid selection`;
+    }
+
+    return response;
   }
 }
