8d2a4759c21cd40fe2ae7ed37c5134c36190bb88
Jeezman <tobiadeyemi91@gmail.com>
2024-09-10 20:43:50 +0100

    create user - add user to ussd module

--- ussd/src/user/user.service.ts
+++ ussd/src/user/user.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { createDid } from 'src/utils/tbd';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = new User();
+    const did = await createDid();
+    user.phoneNumber = createUserDto.phoneNumber;
+    user.did = did.did;
+    user.sessionId = createUserDto.sessionId;
+    user.serviceCode = "444"
+
+    return this.userRepository.save(user);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.userRepository.findOneBy({ id: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    await this.userRepository.delete(id);
   }
 }
